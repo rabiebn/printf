c52dea435023b8d20637fe1da52e24a220f097d0
Souaad OULED-JIA <souadouledjia@gmail.com>
2023-04-23 15:23:10 -0700

    main.h updated

--- main.h
+++ main.h
@@ -1,5 +1,12 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 int _printf(const char *format, ...);
-#endif
+
+#endif /*end _MAIN_H_*/
