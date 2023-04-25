54f12ae1d7a0983f9b6ac34579d75cc21da82b64
Souaad OULED-JIA <souadouledjia@gmail.com>
2023-04-25 07:54:48 -0700

    V7

--- specifier_field.c
+++ specifier_field.c
@@ -2,16 +2,14 @@
 /**
  * get_specifier - find format function equivalent
  * @s: the format string
- * @args: arguments
- * @params: pointer to params_t
- * Return: int number of bytes printed 
+ * Return: int number of bytes printed
  */
 int (*get_specifier(char *s))(va_list args, params_t *params)
 {
 	specifier_t specifiers[] = {
 		{"c", format_char},
 		{"s", format_str},
-		{"%", format_percent}i
+		{"%", format_percent},
 		{NULL, NULL}
 	};
 	int i = 0;
@@ -24,6 +22,20 @@ int (*get_specifier(char *s))(va_list args, params_t *params)
 	}
 	return (NULL);
 }
-int get_print_func(char *s, 
+/**
+ * get_print_func - finds the format function equivalent
+ * @s: the format string
+ * @args: arguments
+ * @params: the parametres of struct
+ * Return: int
+ */
 
+int get_print_func(char *s, va_list args, params_t *params)
+{
+	int (*f)(va_list, params_t *);
+
+	if (f)
+		return (f(args, params));
+	return (0);
+}
 
