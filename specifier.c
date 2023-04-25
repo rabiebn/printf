f08a1e285fa885ac23bf003720270edc7d3431c3
Souaad OULED-JIA <souadouledjia@gmail.com>
2023-04-25 15:30:16 -0700

    intiger added to array

--- specifier.c
+++ specifier.c
@@ -11,6 +11,8 @@ int (*get_specifier(char *s))(va_list args, params_t par)
 		{"c", format_char},
 		{"s", format_str},
 		{"%", format_percent},
+		{"d", format_int},
+		{"i", format_int},
 		/* I'm gonna add more structs here*/
 		{NULL, NULL}
 	};
